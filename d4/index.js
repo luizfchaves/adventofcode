a648580c57f79c2e2d1e7adcc698354b1bbc54a2
Luiz Chaves <lf1405@hotmail.com>
2022-12-04 18:47:54 -0300

    d4 part one

---
+++ d4/index.js
@@ -0,0 +1,48 @@
+const util = require('../util');
+const text = util.readInputTxt('d4');
+
+let fullyContainedAssignments = 0;
+
+text.forEach(item => {
+    const firstElfConstrains = {}
+    const secondElfConstrains = {}
+
+    let pairConstrains = item.split(',');
+
+    firstElfConstrains.start = pairConstrains[0].split("-")[0]
+    firstElfConstrains.end = pairConstrains[0].split("-")[1]
+    secondElfConstrains.start = pairConstrains[1].split("-")[0]
+    secondElfConstrains.end = pairConstrains[1].split("-")[1]
+
+
+    const firstItems = [];
+    const secondItems = [];
+
+
+    for(
+        let i=Math.min(firstElfConstrains.start,secondElfConstrains.start);
+        i<=Math.max(firstElfConstrains.end,secondElfConstrains.end);
+        i++
+    ){
+
+        if(i>= firstElfConstrains.start && i<= firstElfConstrains.end){
+            firstItems.push(i)
+        }
+        if(i>= secondElfConstrains.start && i<= secondElfConstrains.end){
+            secondItems.push(i)
+        }
+    }
+
+    const secondArrayFullyContainsFirst = firstItems.every(item=>secondItems.includes(item));
+    const firstArrayFullyContainsSecond = secondItems.every(item=>firstItems.includes(item));
+
+    if(
+        firstArrayFullyContainsSecond
+        || secondArrayFullyContainsFirst
+    ){
+        fullyContainedAssignments+= 1;
+    }
+
+});
+
+console.log({fullyContainedAssignments})
